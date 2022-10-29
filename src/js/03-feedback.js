b67f0d3a373145bf36e370f58ca10b9603dd1a1c
Alexey7272 <106250222+Alexey7272@users.noreply.github.com>
2022-10-29 17:19:48 +0300

    do hw 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+let formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('textarea'),
+    input: document.querySelector('input'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input',throttle(onFormInput, 500));
+populateTextarea();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function populateTextarea() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(savedMessage);
+    if (savedMessage) {
+        console.log(savedMessage);
+        formData = savedMessage;
+        refs.input.value = formData.email || ""
+        refs.textarea.value = formData.message || "";
+    };
+};
